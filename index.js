46ff37cf169d869b1a214df76cc2c63b4a3f2870
Tom Xu <tomxu89@gmail.com>
2018-01-14 00:20:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,11 +8,23 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine) {
   var personToServe
-  if (katzDeliLine.length = 0) {
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be serverd!"
   } else {
     personToServe = katzDeliLine[0]
     katzDeliLine.shift()
     return personToServe
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var line = "The line is currently: " 
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    for (let i = 1; i < katzDeliLine.length - 1; i++) {
+      line += `${i + 1}. ${katzDeliLine[i]}, `
+    }
+    line += `${i + 1}. ${katzDeliLine[i]}.`
   }
 }
