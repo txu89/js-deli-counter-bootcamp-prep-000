9cf9d030261fd74789916f9baad6d571364d9f67
Tom Xu <tomxu89@gmail.com>
2018-01-14 00:26:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   var line = "The line is currently: " 
-  let i = 1
+  let i = 0
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
